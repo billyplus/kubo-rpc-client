60ed1b0e14b69e1fe85fa5f77a2fc3bb192568ed
billy <me@ybilly.com>
2025-05-30 23:29:05 +0800

    add tests for files/ls

--- files_test.go
+++ files_test.go
@@ -51,3 +51,50 @@ func Test_filesAPI_Stat(t *testing.T) {
 		})
 	}
 }
+
+func Test_filesAPI_List(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		path string
+		opt  []APIOption
+	}
+	// "Connect" to local node
+	addr, err := multiaddr.NewMultiaddr("/ip4/192.168.31.12/tcp/5001")
+	if err != nil {
+		assert.NoError(t, err)
+		return
+	}
+	node, err := rpc.NewApi(addr)
+	if err != nil {
+		assert.NoError(t, err)
+		return
+	}
+	tests := []struct {
+		name    string
+		api     *filesAPI
+		args    args
+		want    *FileListResult
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "root",
+			api:  (FilesAPI(node)),
+			args: args{
+				ctx:  context.Background(),
+				path: "/",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stat, err := tt.api.Stat(tt.args.ctx, tt.args.path, tt.args.opt...)
+			assert.NotNil(t, stat)
+			assert.NoError(t, err)
+
+			got, err := tt.api.List(tt.args.ctx, stat.Hash, tt.args.opt...)
+			assert.NotNil(t, got)
+			assert.NoError(t, err)
+		})
+	}
+}
